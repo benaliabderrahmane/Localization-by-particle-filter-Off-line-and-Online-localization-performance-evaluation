1c39220981343b5f933375bfd0481846f402a322
Abderahmane BENALI <46283596+benaliabderrahmane@users.noreply.github.com>
2021-12-01 20:27:43 +0100

    correct how to save data (filename)  correct the problem with the last segment of the trajectory

--- main.m
+++ main.m
@@ -10,10 +10,10 @@ AllOptions.Selection=["Selection1","Selection2"];
 AllOptions.Distribution=["distance","standard Deviation"];
 AllOptions.SensorsType=["laser","laser front","US"]; %Laser 360° laser1 only from -30 to 210 (240 overall) LAdar for US only.
 AllOptions.NParticles=[5];
-AllOptions.StartPoint=[27.5;50;-pi/2];
+AllOptions.StartPoint=[27.5;15;-pi/2];
 AllOptions.EndPoint=[27;5;0];
-AllOptions.NPp=5;
-AllOptions.MaxSpeed=0.6;
+AllOptions.NPp=2;
+AllOptions.MaxSpeed=2.6;
 AllOptions.NR = 16; %number of rays
 AllOptions.plot = 1; %bool 1 plot 0 do not plot
 
@@ -25,12 +25,10 @@ for i=1:1%length(AllOptions.Likelihood)
         for k=2:length(AllOptions.Distribution)
             for ii = 1:length(AllOptions.NParticles)
                 % for each StudyCase
-                sprintf('Particle Filter using %s %s %s %s %s %s',AllOptions.Likelihood(i),AllOptions.Selection(j),AllOptions.Distribution(k),AllOptions.NParticles,AllOptions.StartPoint,AllOptions.SensorsType,AllOptions.NPp);
                 Options.Likelihood = AllOptions.Likelihood(i);
                 Options.Selection = AllOptions.Selection(j);
                 Options.Distribution = AllOptions.Distribution(k);
                 Options.NParticles = AllOptions.NParticles(ii);
-                Options.StartPoint = AllOptions.StartPoint;
                 Options.SensorsType = AllOptions.SensorsType(2);
                 Options.NPP = AllOptions.NPp;
                 Options.MaxSpeed = AllOptions.MaxSpeed;
@@ -39,21 +37,19 @@ for i=1:1%length(AllOptions.Likelihood)
                 Options.StartPoint=AllOptions.StartPoint;
                 Options.EndPoint=AllOptions.EndPoint;
                 %lunch tests
+                disp('start simulating with:')
+                str = strcat(Options.Likelihood," ",Options.Selection," ",Options.Distribution," ",num2str(Options.NParticles)," ",Options.SensorsType," ",num2str(Options.NPP)," ",num2str(Options.MaxSpeed)," ",num2str(Options.NR))
+                str = regexprep(str,'[^0-9a-zA-Z]','_');
+                sprintf(str);
                 Data = ParticleFilter(Options);
-                disp('save, end case')
+                
                 %% 
                 %add code to save data here
-                t = datetime('now');
-                tt = datestr(t);
-                filename=regexprep(tt,'[^0-9a-zA-Z]','_');
-                str1 = strcat("data\",filename,"_data",".mat");
-                str2 = strcat("data\",filename,"_options",".mat");
-                % we need to use this instead to keep track of options
-                % to be tested and verified later
-                %str = strcat(Options.Likelihood,Options.Selection,num2str(Options.NParticles),num2str(Options.StartPoint),".mat")
-                %save(str,"Data")
-                save(str1,"Data");
-                save(str2,"Options");
+                filename = strcat("data\",str,".mat");
+                save(filename,"Data")
+                %save(str1,"Data");
+                %save(str2,"Options");
+                disp('save, end case')
                 disp('--------------------------------------')
             end
         end
