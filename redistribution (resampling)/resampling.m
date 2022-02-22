fe1fabb8a6b83373b1be71414be7465c85dba516
Abderahmane BENALI <46283596+benaliabderrahmane@users.noreply.github.com>
2022-02-22 11:08:33 +0100

    Add files via upload

--- redistribution (resampling)/resampling.m
+++ redistribution (resampling)/resampling.m
@@ -1,4 +1,4 @@
-function [Particles,OldParticles,OldRobot,vecteurTconvergence] = resampling(Particles,Choice,Obstacles,OldRobot,OldParticles,Nmax,TimeDebut,iteration,vecteurTconvergence,SdX,SdY,SdTheta)
+function [Particles,OldParticles,OldRobot,vecteurTconvergence,PPoids,Indice2_] = resampling(Particles,Choice,Obstacles,OldRobot,OldParticles,Nmax,TimeDebut,iteration,vecteurTconvergence,SdX,SdY,SdTheta,SdwX,SdwY,SdwTheta,Poids,Indice2_)
 %RESAMPLING is the redistribution function 
 %   INPUT:
 %       
@@ -14,8 +14,9 @@ function [Particles,OldParticles,OldRobot,vecteurTconvergence] = resampling(Part
     global Robot
     global N
     global PoseEstime
-    
+    PPoids = Poids;
     if FlagRedistribution==0 %si on est dans la premiere iteration de convergence
+        Indice2_ = 0;
         %T_convergence=toc(TimeDebut);
         %vecteurTconvergence=[vecteurTconvergence,T_convergence];
         %iteration_convergence = iteration; 
@@ -36,7 +37,7 @@ function [Particles,OldParticles,OldRobot,vecteurTconvergence] = resampling(Part
             Distance_robot=norm([Robot.x,Robot.y]-[OldRobot.x,OldRobot.y]);
 
             % s'il y a une difference de  2.5 m, on effectue la redistribution : 
-            if(abs(Distance_Particles-Distance_robot)>2.5)
+            if(abs(Distance_Particles-Distance_robot)>1)
                 N=Nmax;
                 particles1=Particles_generator(26.5747,29.02,-0.269984,55,-pi,pi,floor(N/2),Obstacles);
                 particles2=Particles_generator(-5,26.5747,-0.269984,3,-pi,pi,N-floor(N/2),Obstacles);
@@ -52,7 +53,7 @@ function [Particles,OldParticles,OldRobot,vecteurTconvergence] = resampling(Part
                 FlagRedistribution=0;
             end
         case "STANDARD DEVIATION"
-            if (SdX<0.5 && SdY<0.5 && SdTheta<0.2)
+            if (SdX<0.2 && SdY<0.2 && SdTheta<0.1)
                 N=Nmax;
                 particles1=Particles_generator(26.5747,29.02,-0.269984,55,-pi,pi,floor(N/2),Obstacles);
                 particles2=Particles_generator(-5,26.5747,-0.269984,11.53,-pi,pi,N-floor(N/2),Obstacles);
@@ -62,7 +63,23 @@ function [Particles,OldParticles,OldRobot,vecteurTconvergence] = resampling(Part
                 Particles.theta=particles(3,:);
                 Indice_ = 0;
                 FlagRedistribution=0;
-            end 
+            end
+            case "WHEIGHTED STANDARD DEVIATION"
+                max(PPoids)
+                if ((SdwX<0.3 && SdwY<0.3) )
+                    N=Nmax;
+                    particles1=Particles_generator(26.5747,29.02,-0.269984,55,-pi,pi,floor(N/2),Obstacles);
+                    particles2=Particles_generator(-5,26.5747,-0.269984,11.53,-pi,pi,N-floor(N/2),Obstacles);
+                    particles=[particles1,particles2];
+                    Particles.x=particles(1,:);
+                    Particles.y=particles(2,:);
+                    Particles.theta=particles(3,:);
+                    PPoids = ones(N,1)/N;
+                    Indice_ = 0;
+                    FlagRedistribution=0;
+                else 
+                    Indice2_=Indice2_+1;
+                end
         otherwise
             error("redistribution function is not defined!! add it in redistribution (resampling)/resampling.m")
         end
